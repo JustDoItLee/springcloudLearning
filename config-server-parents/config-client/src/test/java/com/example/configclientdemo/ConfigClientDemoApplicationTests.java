c89c8e97f95bd6a0ab99b6ceab9d823259f3bf6e
judtdoitlee <657994434@qq.com>
2020-08-16 17:57:23 +0800

    配置中心

---
+++ config-server-parents/config-client/src/test/java/com/example/configclientdemo/ConfigClientDemoApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example.configclientdemo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientDemoApplicationTests {
+
+
+}
---
+++ config-server-parents/config-server/src/test/java/com/example/configserverdemo/ConfigServerDemoApplicationTests.java
@@ -0,0 +1,8 @@
+package com.example.configserverdemo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerDemoApplicationTests {
+
+}
